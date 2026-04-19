0438fb7db486467595e9df9a5ea4a73c9a1475a3
Dave MacStudio <sodekd@gmail.com>
2026-04-19 09:17:58 +0200

    feat: Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "Živý žebříček ČTS mládež",
+  title: "Live Ranking - ČTS mládež",
   description: "Aktuální žebříček mladšího žactva, staršího žactva a dorostu",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="cs">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
