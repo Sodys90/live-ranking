6c0ddad21839ad40f27abb7c7584881562aff529
Dave MacStudio <sodekd@gmail.com>
2026-04-20 11:36:58 +0200

    ci: ruční aktualizace přes aktualizuj.sh

--- aktualizuj.sh
+++ aktualizuj.sh
@@ -2,11 +2,11 @@
 cd ~/Desktop/tenis-zebricky
 source ~/venv-grant/bin/activate
 
-echo "1/4 České žebříčky..."
+echo "1/4 České žebříčky (trvá hodiny)..."
 caffeinate -i python3 init_hraci_s_body.py
 
 echo "2/4 ITF scraper..."
-python3 scraper_itf.py
+caffeinate -i python3 scraper_itf.py
 
 echo "3/4 Přepočet žebříčku..."
 python3 -c "from scraper_turnaje import prepocitej_zebricky; prepocitej_zebricky()"
